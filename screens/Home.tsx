1297aa3e4aca446374e8c22cae5c7d737e046327
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-15 21:24:21 -0400

    Update Home.tsx

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -18,8 +18,18 @@ import { VictoryPie } from "victory-native";
 import { Svg } from "react-native-svg";
 
 import { COLORS, FONTS, SIZES, icons } from "../constants";
+import { categoriesData } from "../data";
 
 const Home = () => {
+  const categoryListHeightAnimationValue = useRef(
+    new Animated.Value(115)
+  ).current;
+
+  const [categories, setCategories] = useState(categoriesData);
+  const [viewMode, setViewMode] = useState("chart");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showMoreToggle, setShowMoreToggle] = useState(false);
+
   return (
     <View>
       <Text>Home</Text>
