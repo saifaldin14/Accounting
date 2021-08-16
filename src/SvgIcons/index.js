337b8b6a4d184a7dd0075a4129061103f6796a77
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-15 20:46:15 -0400

    Restore old version

--- src/SvgIcons/index.js
+++
@@ -1,7 +0,0 @@
-export { default as PortfolioIcon } from "./Portfolio";
-export { default as Pulse } from "./Pulse";
-export { default as Market } from "./Market";
-export { default as News } from "./News";
-export { default as Menu } from "./Menu";
-export { default as Add } from "./Add";
-export { default as Close } from "./Close";
--- store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import transactionReducer from "./transactionReducer";
-
-export default combineReducers({
-  trs: transactionReducer,
-});
