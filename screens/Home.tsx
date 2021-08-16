69f14272256437f97ec60cc45d746b3681549826
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-15 21:12:36 -0400

    Create Home

---
+++ screens/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SIZES, FONTS, COLORS, icons } from "../constants";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
